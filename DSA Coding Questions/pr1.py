a668d7658d7f5f728809789a245015e55b6261f5
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-12-07 16:08:38 +0530

    Add files via upload

--- DSA Coding Questions/pr1.py
+++ DSA Coding Questions/pr1.py
@@ -20,3 +20,23 @@ def triangle(s,n):
 s='ABCDE'
 n=len(s)
 triangle(s,n)
+
+
+def triangle(n):
+    k=65
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print(chr(i+k),end=' ')
+        print()
+        print()
+triangle(5)
+
+def triangle(n):
+    k=65
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print(chr(i+k),end=' ')
+        
+        print()
+        print()
+triangle(5)
