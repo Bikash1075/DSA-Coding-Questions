5ed51acf0b9114f7de400e12318ba162e3e9a733
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-12-09 22:14:48 +0530

    Add files via upload

--- DSA Coding Questions/pr5.py
+++ DSA Coding Questions/pr5.py
@@ -5,14 +5,25 @@
 # Input: arr[] = {1, 2, 3, 4, 5}
 
 # Output: arr[] = {5, 1, 2, 3, 4}
-
-def rotate(arr,n):
+def rotation(arr,n):
     x=arr[n-1]
-    for i in range (n-1,0,-1):
+    for i in range(n-1,0,-1):
         arr[i]=arr[i-1]
     arr[0]=x
-arr={1,2,3,4,5}
-n=len(arr)
-rotate(arr,n)
-print(arr)
+    return arr
+arr=[1,2,3,4,5]
+n= len(arr)
+print(rotation(arr,n))
+
+def circularlly_rotated(arr,n):
+    temp=[]
+    for j in range(n):
+        temp.append(arr[j - 1])
+    arr=temp[:]
+    return(arr)
+arr=[1,2,3,4,5]
+n= len(arr)
+print(circularlly_rotated(arr,n))
+
+
 
