6a705d83ede6cb763493a50b1856cb9c1c887759
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-12-11 18:41:54 +0530

    Update pr5.py

--- DSA Coding Questions/pr5.py
+++ DSA Coding Questions/pr5.py
@@ -25,23 +25,3 @@ def circularlly_rotated(arr,n):
 arr=[20,50,47,89,56,23]
 n= len(arr)
 print(circularlly_rotated(arr,n))
-
-def rotate(arr,n):
-    a=arr[n-1]
-    for i in range(n-1,0,-1):
-        arr[i]=arr[i-1]
-    arr[0]=a
-    return arr
-arr=[1,2,3,4,5]
-n=len(arr)
-print(rotate(arr,n))
-
-def rotate_by_one(arr,n):
-    a=arr[n-1]
-    for i in range(n-1,0,-1):
-        arr[i]=arr[i-1]
-    arr[0]=a
-    return arr
-arr=[25,78,45,15,48,79,46]
-n= len(arr)
-rotate_by_one(arr,n)
