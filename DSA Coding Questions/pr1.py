f1982b6d6745abe5013a97db8c65f415efeb5094
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-12-11 14:13:28 +0530

    Add files via upload

--- DSA Coding Questions/pr1.py
+++ DSA Coding Questions/pr1.py
@@ -11,9 +11,10 @@
 
 # E E E E E
 
+# 1st approach
 def triangle(s,n):
     for i in range(n):
-        for j in range(1,i+2):
+        for j in range(0,i+1):
             print(s[i],end=' ')
         print()
         print()
@@ -21,21 +22,27 @@ s='ABCDE'
 n=len(s)
 triangle(s,n)
 
-
+# 2nd approach
 def triangle(n):
     k=65
     for i in range(0,n):
-        for j in range(0,i+1):
+        for j in range(i+1):
             print(chr(i+k),end=' ')
         print()
         print()
 triangle(5)
 
+# 3rd approach
 def triangle(n):
-    k=65
-    for i in range(0,n):
-        for j in range(0,i+1):
-            print(chr(i+k),end=' ')
+    for i in range(65,70):
+        for j in range(65,i+1):
+            print(chr(i),end=' ')
         print()
         print()
 triangle(5)
+
+# 4th approach
+n,k=5,65
+for i in range(0,n):
+    print((chr(k+i)+' ')*(i+1))
+    print()
